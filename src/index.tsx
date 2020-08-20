08129193331f6de9ad43a630254630b9c6e72dc4
Bart Langelaan <langelaanbart@gmail.com>
2020-08-20 13:34:51 +0200

    Add sounds

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,58 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { render } from "react-dom";
 import useGeolocation from "react-hook-geolocation";
 import { getDistance } from "geolib";
 import screenfull from "screenfull";
+import useSound from "use-sound";
+import closerSound from "./closer.wav";
+import updateSound from "./update.mp3";
 
 function App() {
+  const [playCloserSound] = useSound(closerSound);
+
+  const [playFurtherSound] = useSound(updateSound);
+  const [playUpdateSound] = useSound(updateSound, { volume: 0.2 });
   const geolocation = useGeolocation({ enableHighAccuracy: true });
   const now = useNow();
+
+  const lastDistance = useRef<number>();
+  const lastTimestamp = useRef<number>();
+
+  const distance =
+    geolocation.latitude &&
+    getDistance(geolocation, {
+      latitude: 52.002889,
+      longitude: 4.437897,
+    });
+
+  const timeAgo =
+    geolocation.timestamp &&
+    Math.max(0, Math.floor((now - geolocation.timestamp) / 1000));
+
+  useEffect(() => {
+    if (lastDistance.current && distance && distance < lastDistance.current) {
+      playCloserSound();
+    } else if (
+      lastDistance.current &&
+      distance &&
+      distance > lastDistance.current
+    ) {
+      playFurtherSound();
+    } else if (
+      lastTimestamp.current &&
+      geolocation.timestamp &&
+      geolocation.timestamp > lastTimestamp.current
+    ) {
+      playUpdateSound();
+    } else if (
+      typeof lastTimestamp.current === "undefined" ||
+      typeof lastDistance.current === "undefined"
+    ) {
+      playUpdateSound();
+    }
+    lastTimestamp.current = geolocation.timestamp || undefined;
+    lastDistance.current = distance || undefined;
+  });
 
   if (!geolocation.latitude) {
     if (geolocation.error) {
@@ -14,13 +60,6 @@ function App() {
     }
     return <>Lokaliseren...</>;
   }
-
-  const distance = getDistance(geolocation, {
-    latitude: 52.002889,
-    longitude: 4.437897,
-  });
-
-  const timeAgo = Math.max(0, Math.floor((now - geolocation.timestamp) / 1000));
 
   return (
     <>
@@ -36,7 +75,7 @@ function App() {
 }
 
 const appDiv = document.getElementById("app");
-if (!appDiv) throw new Error('No appdiv');
+if (!appDiv) throw new Error("No appdiv");
 
 render(<App />, appDiv);
 
