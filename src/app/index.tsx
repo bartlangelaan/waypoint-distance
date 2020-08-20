3627846824cd0bdae02fd38765dea87ab7ecac13
Bart Langelaan <langelaanbart@gmail.com>
2020-08-20 17:55:01 +0200

    Move tracker to its own component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,84 +1,14 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import { render } from "react-dom";
-import useGeolocation from "react-hook-geolocation";
-import { getDistance } from "geolib";
 import screenfull from "screenfull";
-import useSound from "use-sound";
-import closerSound from "../sounds/closer.wav";
-import updateSound from "../sounds/update.mp3";
 import styles from "./index.css";
+import { Tracker } from "./tracker";
 
 function App() {
-  const [playCloserSound] = useSound(closerSound);
-
-  const [playFurtherSound] = useSound(updateSound);
-  const [playUpdateSound] = useSound(updateSound, { volume: 0.2 });
-  const geolocation = useGeolocation({ enableHighAccuracy: true });
-  const now = useNow();
-
-  const lastDistance = useRef<number | null>(null);
-  const lastTimestamp = useRef<number | null>(null);
-
-  const distance =
-    geolocation.latitude &&
-    getDistance(geolocation, {
-      latitude: 52.002889,
-      longitude: 4.437897,
-    });
-
-  const timeAgo =
-    geolocation.timestamp &&
-    Math.max(0, Math.floor((now - geolocation.timestamp) / 1000));
-
-  useEffect(() => {
-    if (
-      lastDistance.current !== null &&
-      distance !== null &&
-      distance < lastDistance.current
-    ) {
-      playCloserSound();
-    } else if (
-      lastDistance.current !== null &&
-      distance !== null &&
-      distance > lastDistance.current
-    ) {
-      playFurtherSound();
-    } else if (
-      lastTimestamp.current !== null &&
-      geolocation.timestamp !== null &&
-      geolocation.timestamp > lastTimestamp.current
-    ) {
-      playUpdateSound();
-    } else if (
-      (lastTimestamp.current === null && geolocation.timestamp !== null) ||
-      (lastDistance.current === null && distance !== null)
-    ) {
-      playUpdateSound();
-    }
-    lastTimestamp.current = geolocation.timestamp;
-    lastDistance.current = distance;
-  });
-
-  if (!geolocation.latitude || timeAgo === null) {
-    if (geolocation.error) {
-      return <>Error: {geolocation.error}</>;
-    }
-    return <>Lokaliseren...</>;
-  }
-
   return (
     <>
       <div className={styles.header}>Pirate Camp 2020</div>
-      <div className={styles.distance}>
-        <div className={styles.number}>{distance}</div>
-        <div className={styles.distanceSub}>meters to go</div>
-      </div>
-      <div className={styles.timeAgo}>
-        {timeAgo.toString().padStart(2, "0")}s old
-      </div>
-      <div className={styles.accuracy}>
-        Accuracy: {Math.ceil(geolocation.accuracy)}m
-      </div>
+      <Tracker latitude={52.002889} longitude={4.437897} />
     </>
   );
 }
@@ -87,17 +17,6 @@ const appDiv = document.getElementById("app");
 if (!appDiv) throw new Error("No appdiv");
 
 render(<App />, appDiv);
-
-function useNow() {
-  const [now, setNow] = useState(new Date());
-  useEffect(() => {
-    const i = setInterval(() => {
-      setNow(new Date());
-    }, 1000);
-    return () => clearInterval(i);
-  });
-  return now.getTime();
-}
 
 appDiv.addEventListener("click", () => {
   if (screenfull.isEnabled) {
---
+++ src/app/tracker/index.tsx
@@ -0,0 +1,96 @@
+import React, { useState, useEffect, useRef } from "react";
+import useGeolocation from "react-hook-geolocation";
+import { getDistance } from "geolib";
+import useSound from "use-sound";
+import closerSound from "../../sounds/closer.wav";
+import updateSound from "../../sounds/update.mp3";
+import styles from "./index.css";
+
+interface Props {
+  latitude: number;
+  longitude: number;
+}
+export function Tracker(props: Props) {
+  const [playCloserSound] = useSound(closerSound);
+
+  const [playFurtherSound] = useSound(updateSound);
+  const [playUpdateSound] = useSound(updateSound, { volume: 0.2 });
+  const geolocation = useGeolocation({ enableHighAccuracy: true });
+  const now = useNow();
+
+  const lastDistance = useRef<number | null>(null);
+  const lastTimestamp = useRef<number | null>(null);
+
+  const distance =
+    geolocation.latitude &&
+    getDistance(geolocation, {
+      latitude: props.latitude,
+      longitude: props.longitude,
+    });
+
+  const timeAgo =
+    geolocation.timestamp &&
+    Math.max(0, Math.floor((now - geolocation.timestamp) / 1000));
+
+  useEffect(() => {
+    if (
+      lastDistance.current !== null &&
+      distance !== null &&
+      distance < lastDistance.current
+    ) {
+      playCloserSound();
+    } else if (
+      lastDistance.current !== null &&
+      distance !== null &&
+      distance > lastDistance.current
+    ) {
+      playFurtherSound();
+    } else if (
+      lastTimestamp.current !== null &&
+      geolocation.timestamp !== null &&
+      geolocation.timestamp > lastTimestamp.current
+    ) {
+      playUpdateSound();
+    } else if (
+      (lastTimestamp.current === null && geolocation.timestamp !== null) ||
+      (lastDistance.current === null && distance !== null)
+    ) {
+      playUpdateSound();
+    }
+    lastTimestamp.current = geolocation.timestamp;
+    lastDistance.current = distance;
+  });
+
+  if (!geolocation.latitude || timeAgo === null) {
+    if (geolocation.error) {
+      return <>Error: {geolocation.error}</>;
+    }
+    return <>Lokaliseren...</>;
+  }
+
+  return (
+    <>
+      <div className={styles.distance}>
+        <div className={styles.number}>{distance}</div>
+        <div className={styles.distanceSub}>meters to go</div>
+      </div>
+      <div className={styles.timeAgo}>
+        {timeAgo.toString().padStart(2, "0")}s old
+      </div>
+      <div className={styles.accuracy}>
+        Accuracy: {Math.ceil(geolocation.accuracy)}m
+      </div>
+    </>
+  );
+}
+
+function useNow() {
+  const [now, setNow] = useState(new Date());
+  useEffect(() => {
+    const i = setInterval(() => {
+      setNow(new Date());
+    }, 1000);
+    return () => clearInterval(i);
+  });
+  return now.getTime();
+}
