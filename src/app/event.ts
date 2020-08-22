c6eeab43e7139c76dec33c5c918570d7a54f93a5
Bart Langelaan <langelaanbart@gmail.com>
2020-08-22 03:22:57 +0200

    Subtract

--- src/app/event.ts
+++ src/app/event.ts
@@ -1,8 +1,8 @@
 const user = Math.ceil(Math.random() * 1000);
-let event = 0;
+let event = 1000000;
 
 export function log(e: string) {
-  event += 1;
+  event -= 1;
   gtag("event", `${event.toString().padStart(6, "0")} - ${e}`, {
     event_category: `User ${user} (v${process.env.GITHUB_RUN_NUMBER || 0})`,
   });
