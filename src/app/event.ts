d4ec1ca7c16ab3bc647d69e86bd9f4b4752de1cf
Bart Langelaan <langelaanbart@gmail.com>
2020-08-22 03:02:55 +0200

    Add version to events

--- src/app/event.ts
+++ src/app/event.ts
@@ -6,7 +6,7 @@ let event = SSL_OP_SSLEAY_080_CLIENT_DH_BUG;
 export function log(e: string) {
   event += 1;
   gtag("event", `${event.toString().padStart(6)} - ${e}`, {
-    event_category: `User ${user}`,
+    event_category: `User ${user} (v${process.env.GITHUB_RUN_NUMBER || 0})`,
   });
 }
 
